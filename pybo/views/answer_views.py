189ec56a8f0e0fbcd72be9d34ed46d69cbb9264d
공상혁 <kkong0123@naver.com>
2022-12-16 19:33:42 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/views/answer_views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, get_object_or_404, redirect, resolve_url
+from ..models import Question, Answer
+from django.utils import timezone
+from ..forms import AnswerForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+@login_required(login_url = 'common:login')
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == 'POST': 
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('{}#answer_{}'.format(resolve_url( 'pybo:detail', question_id= question.id), answer.id))
+    else:
+        form = AnswerForm()
+    context = {'question':question, 'form':form}
+    return render(request, 'pybo/question_detail.html', context)
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    answer = get_object_or_404(Answer, pk = answer_id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('{}#answer_{}'.format(resolve_url( 'pybo:detail', question_id= answer.question.id), answer.id))
+    else:
+        form = AnswerForm(instance=answer)
+    return render(request, 'pybo/answer_form.html', {'form':form})
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제권한이 없습니다.')
+    else: 
+        answer.delete()
+    return redirect('pybo:detail', answer.question.id)
+
+@login_required(login_url="common:login")
+def answer_vote(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user == answer.author:
+        messages.error(request, '본인의 글에는 추천할 수 없습니다.')
+    else:
+        answer.voter.add(request.user)
+    return redirect('{}#answer_{}'.format(resolve_url( 'pybo:detail', question_id= answer.question.id), answer.id))
